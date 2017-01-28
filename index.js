75d77677392d98e2792dd5cc12378025c4def8c7
Heikki Ritola <heikki.ritola@reaktor.com>
2017-01-28 03:25:25 +0200

    Use mode buttons to select surface mode

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ scs3d.version.stream.log('Stanton SCS.3d')
 scs3d.version.stream.onValue(reset)
 
 function reset () {
-  scs3d.extinguishAllLeds()
-
   const ledsOnY = (y) => { return R.values(R.filter(R.propEq('y', y), scs3d.ledPositions)) }
   const setLedOnY = (y, color) => {
     R.forEach((button) => { if (button.key) scs3d.sendNoteOn(button.key, color) }, ledsOnY(y))
@@ -44,11 +42,7 @@ function reset () {
     scs3d.leds.deckB(0x00)
     scs3d.surface.leds.setAllBlack()
     scs3d.surface.leds.setCircleAll(0x00)
-    scs3d.button.mode.fx.ledOnPress(0x02, 0x01)
-    scs3d.button.mode.eq.ledOnPress(0x02, 0x01)
-    scs3d.button.mode.loop.ledOnPress(0x02, 0x01)
-    scs3d.button.mode.trig.ledOnPress(0x02, 0x01)
-    scs3d.button.mode.vinyl.ledOnPress(0x02, 0x01)
+    scs3d.button.mode.setAll(2)
     scs3d.button.mode.deck.ledOnPress(0x02, 0x01)
     scs3d.button.soft.b11.ledOnPress(0x02, 0x01)
     scs3d.button.soft.b12.ledOnPress(0x02, 0x01)
@@ -58,50 +52,33 @@ function reset () {
     scs3d.button.transport.cue.ledOnPress(0x00, 0x01)
     scs3d.button.transport.sync.ledOnPress(0x00, 0x01)
     scs3d.button.transport.tap.ledOnPress(0x00, 0x01)
+    scs3d.leds.pitchBlue(0x01)
     scs3d.leds.stanton(0x01)
   })
 
-  const loopThrough = (led) => {
-    Bacon.repeatedly(300, led.colors).onValue(
-        (c) => { console.log(c); scs3d.sendNoteOn(led.key, c) }
-      )
-  }
-
-  const loopThroughCC = (controller) => {
-    Bacon.repeatedly(300, R.range(70, 128)).onValue(
-        (c) => { console.log(c); scs3d.sendCC(controller, c) }
-      )
-  }
-
-// loopThroughCC(0x07)
-
- // loopThrough(scs3d.surface.ring[0])
-
-/*
-  scs3d.leds.deckA(0x00)
-  scs3d.leds.deckB(0x00)
-  scs3d.surface.leds.setAllBlack()
-  scs3d.surface.leds.setCircleAll(0x00)
-  scs3d.button.mode.fx.ledOnPress(0x02, 0x01)
-  scs3d.button.mode.eq.ledOnPress(0x02, 0x01)
-  scs3d.button.mode.loop.ledOnPress(0x02, 0x01)
-  scs3d.button.mode.trig.ledOnPress(0x02, 0x01)
-  scs3d.button.mode.vinyl.ledOnPress(0x02, 0x01)
-  scs3d.button.mode.deck.ledOnPress(0x02, 0x01)
-  scs3d.button.soft.b11.ledOnPress(0x02, 0x01)
-  scs3d.button.soft.b12.ledOnPress(0x02, 0x01)
-  scs3d.button.soft.b13.ledOnPress(0x02, 0x01)
-  scs3d.button.soft.b14.ledOnPress(0x02, 0x01)
-  scs3d.button.transport.play.ledOnPress(0x00, 0x01)
-  scs3d.button.transport.cue.ledOnPress(0x00, 0x01)
-  scs3d.button.transport.sync.ledOnPress(0x00, 0x01)
-  scs3d.button.transport.tap.ledOnPress(0x00, 0x01)
-  scs3d.leds.logo(0x01)
-  scs3d.test()
-*/
+  scs3d.button.mode.vinyl.stream.onValue(() => {
+    scs3d.button.mode.setAll(2)
+    scs3d.button.mode.vinyl.setLed(1)
+    scs3d.surface.mode.setCircle()
+  })
+  scs3d.button.mode.eq.stream.onValue(() => {
+    scs3d.button.mode.setAll(2)
+    scs3d.button.mode.eq.setLed(1)
+    scs3d.surface.mode.setSliders()
+  })
+  scs3d.button.mode.fx.stream.onValue(() => {
+    scs3d.button.mode.setAll(2)
+    scs3d.button.mode.fx.setLed(1)
+    scs3d.surface.mode.setSliders()
+  })
+  scs3d.button.mode.trig.stream.onValue(() => {
+    scs3d.button.mode.setAll(2)
+    scs3d.button.mode.trig.setLed(1)
+    scs3d.surface.mode.setButtons()
+  })
+  scs3d.button.mode.loop.stream.onValue(() => {
+    scs3d.button.mode.setAll(2)
+    scs3d.button.mode.loop.setLed(1)
+    scs3d.surface.mode.setButtons()
+  })
 }
-
-scs3d.button.transport.play.stream.onValue(() => { scs3d.surface.mode.setCircle() })
-scs3d.button.transport.cue.stream.onValue(() => { scs3d.surface.mode.setSliders() })
-scs3d.button.transport.sync.stream.onValue(() => { scs3d.surface.mode.setButtons() })
-scs3d.button.transport.tap.stream.onValue(() => { scs3d.button.mode.setAll(0x02) })
